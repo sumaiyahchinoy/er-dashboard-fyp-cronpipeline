9445233a4c8b5bf63903eb4a53c63226a120a8eb
qiblatainf <qiblatain@live.com>
2023-04-11 22:13:15 +0500

    summary statistics and adfuller test done

--- TimeSeriesModelling.py
+++ TimeSeriesModelling.py
@@ -5,4 +5,11 @@ onedrive_link = 'https://1drv.ms/x/s!AsCp_kE1E5Md6U4PwNcWkXp_SnEL?e=3gStJc'
 link = create_onedrive_directdownload(onedrive_link)
 df = pd.read_excel(link)
 
-print(df.head(5))
+#removing anomalies
+df = df[df.Triage_Datetime != df.ER_No]
+df.pop(df.columns[0])
+
+#fetching date from the datetime column
+df['Triage_Datetime'] = pd.to_datetime(df['Triage_Datetime'])
+df['Triage_Datetime'] = df['Triage_Datetime'].dt.date
+df.head(5)
