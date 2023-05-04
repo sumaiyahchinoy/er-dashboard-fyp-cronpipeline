9eee6d004c9bcb8bc1ca96d92d1dee2b6125a367
qiblatainf <qiblatain@live.com>
2023-05-04 19:06:30 +0500

    Links appended

---
+++ append_dirty_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from access_onedrive import create_onedrive_directdownload
+   
+data_files = ['https://1drv.ms/x/s!AsCp_kE1E5Md510L7cgZfxy_ifCq?e=bNj2eY', 'https://1drv.ms/x/s!AsCp_kE1E5Md6TUUuL5DtjrFTggO?e=xA3jaE', 'https://1drv.ms/x/s!AsCp_kE1E5Md6TSDbgcW_MFQzySB?e=oWsFUm']
+# print(data_files[1])
+# print(len(data_files))
+
+# file_link = []
+# for file in data_files:
+#     file_link.append(create_onedrive_directdownload(file))
+# # print(file_link)
+
+data = pd.DataFrame()
+for file in data_files:
+    data.append(pd.read_excel(create_onedrive_directdownload(file)))
+
+# data = pd.read_excel(link)
+
+# df = df_diagnosis.append(df_null)
+# df = df.sort_index()
+    
